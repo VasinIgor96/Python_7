bc1b3ba5f9faa48eb243ac0f46da5ff50834e5b0
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-20 23:32:29 +0300

    Ex_4 Task done

--- Python_7/Python_7.py
+++ Python_7/Python_7.py
@@ -1,1 +1,12 @@
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
+n = int(input("Введіть номер елемента послідовності Фібоначчі: "))
+
+result = fibonacci(n)
+print(f"n-ий елемент послідовності Фібоначчі: {result}")
