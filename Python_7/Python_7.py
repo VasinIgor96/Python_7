a7de35e89a7adb63ef0c17c8d0b32c100d4666dd
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-20 23:17:37 +0300

    Ex_2 Task done

--- Python_7/Python_7.py
+++ Python_7/Python_7.py
@@ -1,1 +1,11 @@
+import re
 
+def count_vowels_consonants(s):
+    vowels = ''.join(re.findall(r'[aeiouаеиоуюяіїє]', s, re.IGNORECASE | re.UNICODE))
+    consonants = ''.join(re.findall(r'[bcdfghjklmnpqrstvwxyzбвгґджзйклмнпрстфхцчшщ]', s, re.IGNORECASE | re.UNICODE))
+    return len(vowels), len(consonants)
+
+s = input("Введіть рядок: ")
+vowels, consonants = count_vowels_consonants(s)
+print(f"Кількість голосних: {vowels}")
+print(f"Кількість приголосних: {consonants}")
