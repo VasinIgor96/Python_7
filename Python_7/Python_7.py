e10a86ec133fbea776cedfa73216ef5786de086e
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-20 21:44:14 +0300

    Ex_1 Task done

--- Python_7/Python_7.py
+++ Python_7/Python_7.py
@@ -1,1 +1,7 @@
 
+def calculate_mean(numbers):
+    return sum(numbers) / len(numbers)
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+mean = calculate_mean(my_list)
+print("Середнє арифметичне значення чисел: ",mean)
