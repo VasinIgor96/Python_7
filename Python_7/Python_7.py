92a2b6299222d4e440cfe62726080a635a79efb5
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-20 23:57:14 +0300

    Ex_8 Task done

--- Python_7/Python_7.py
+++ Python_7/Python_7.py
@@ -1,1 +1,7 @@
 
+def sort_tuples_by_age(lst):
+    return sorted(lst, key=lambda x: x[1])
+
+people = [('Олександр', 25), ('Олена', 20), ('Іван', 30), ('Вікторія', 15),('Сергій', 10)]
+sorted_people = sort_tuples_by_age(people)
+print(sorted_people)
