7bc4f794bcaa78bbdb6a45024f6dd22f33aece5c
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-21 00:01:55 +0300

    Ex_9 Task done

--- Python_7/Python_7.py
+++ Python_7/Python_7.py
@@ -1,1 +1,10 @@
 
+def get_first_last_chars(s):
+    if len(s) < 6:
+        return "Недостатньо символів"
+    return s[:3] + s[-3:]
+
+s = input("Введіть рядок: ")
+
+result = get_first_last_chars(s)
+print(result)
