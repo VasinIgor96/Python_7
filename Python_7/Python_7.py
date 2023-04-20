c5cceb4ea28a3ee31caa1a46bde9891ebc214240
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-20 23:23:04 +0300

    Ex_3 Task done

--- Python_7/Python_7.py
+++ Python_7/Python_7.py
@@ -1,1 +1,10 @@
 
+def unique_elements_in_order(lst):
+    unique_lst = []
+    for item in lst:
+        if item not in unique_lst:
+            unique_lst.append(item)
+    return unique_lst
+lst = [1, 2, 4, 3, 3, 3, 4, 4, 5, 6, 9, ]
+unique_lst = unique_elements_in_order(lst)
+print(unique_lst)
