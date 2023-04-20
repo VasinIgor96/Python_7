7b8f901b7598897b1da7093ebacb0cfff1ea22d4
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-20 23:49:42 +0300

    Ex_7 task done

--- Python_7/Python_7.py
+++ Python_7/Python_7.py
@@ -1,1 +1,11 @@
 
+def find_divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+# Перевірка
+num = int(input("Введіть ціле число: "))
+print("Дільники числа", num, ":", find_divisors(num))
