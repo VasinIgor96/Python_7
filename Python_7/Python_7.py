875f3a2833caa43c3d4dfbb2b9fb8b2c3b353e65
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-20 23:42:10 +0300

    Ex_5 task done

--- Python_7/Python_7.py
+++ Python_7/Python_7.py
@@ -1,1 +1,6 @@
 
+def capitalize_first_letters(s):
+    return ' '.join(word.capitalize() for word in s.split())
+s = input("Введіть рядок: ")
+capitalized = capitalize_first_letters(s)
+print(capitalized)
