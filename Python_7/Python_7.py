21e57768d83878e73a546cacf695c1502fc27cef
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-20 23:46:02 +0300

    Ex_6 Task done

--- Python_7/Python_7.py
+++ Python_7/Python_7.py
@@ -1,1 +1,10 @@
 
+def dot_product(list1, list2):
+    if len(list1) != len(list2):
+        return None
+    return sum([list1[i] * list2[i] for i in range(len(list1))])
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+result = dot_product(list1, list2)
+print("Скалярний добуток:",result)
